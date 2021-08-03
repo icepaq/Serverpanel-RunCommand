308e4fc9bd8bdf45381acf2726992b3d0a62e801
Anton <62949848+icepaq@users.noreply.github.com>
2021-08-03 14:49:36 -0400

    replace hard coded password with environment variable

--- src/main/java/com/icepaq/ServerManagementAPIEndpoint/Codes.java
+++ src/main/java/com/icepaq/ServerManagementAPIEndpoint/Codes.java
@@ -4,5 +4,5 @@ public class Codes {
 	String host_name = "jdbc:mysql://localhost:3306/";
 	String host = "jdbc:mysql://localhost:3306"; //Used only for setting up the database.
 	String db_username = "root";
-	String db_password = "java_test";
+	String db_password = System.getenv("DATAMYSQLPASS");
 }
